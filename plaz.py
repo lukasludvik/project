cb999a774188095fd88d84e947ebe3b5c488ff00
Lukas Ludvik <lukas.ludvik@yahoo.com>
2022-09-20 08:13:21 +0200

    first commit

---
+++ plaz.py
@@ -0,0 +1,25 @@
+class Table:
+     def __init__(self, x, y, start, end, pl):
+         self.x = x
+         self.y = y
+         self.aval = abs(self.x - start[pl] + (self.y - start[pl + 1]))
+         self.bval = abs(self.x - end[pl] + (self.y - end[pl + 1]))
+         self.cval = self.aval + self.bval
+
+start = []
+end = []
+sand = []
+
+area = int(input()) #5
+
+#9Making quicksand
+while True:
+    x, y = [int(x) for x in input().split() if input()]
+    start.append(x)
+    start.append(y)
+    if isinstance(x, int):
+        continue
+    else:
+        break
+
+print(x, y)
---
+++ test.py
@@ -0,0 +1,8 @@
+start = ()
+
+while True:
+    x, y = [int(x) for x in input().split()]
+    start.append(x)
+    start.append(y)
+
+print(x, y)
